b1cfb41596d64ee9b174e1c9093998edb0840725
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2026-09-05 21:49:01 +0200

    Create BinarySearch.cs

---
+++ BinarySearch.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+namespace CSES_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -1, 0, 3, 5, 9, 12 };
+            int target = 2;
+
+            Console.WriteLine(binarySearch(arr, target));
+        }
+
+        static int binarySearch(int[] arr, int target)
+        {
+            arr.Sort();
+
+            int left = 0, right = arr.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == target)
+                    return mid;
+                else if (arr[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
